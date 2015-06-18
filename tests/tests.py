22cab20b24258583e2b7571f978390940346f9ec
imlonghao <shield@fastmail.com>
2015-06-18 21:44:07 +0800

    Complete the test file

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
 from portstat import portstat
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+confPath = path.join(here, 'portstat.conf')
+
+version = portstat.version()
+portGroups = portstat.getConfig(confPath)
+portstat.sync(portGroups)
+portstat.upload(portGroups)
+
+portstat.main()
