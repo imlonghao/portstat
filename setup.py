7a72320225da79ed38919f9fe5551114f04e872c
imlonghao <shield@fastmail.com>
2015-06-18 21:34:56 +0800

    Add long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='portstat',
     version='0.0.1',
-    keywords= ('port', 'monitor', 'traffic'),
+    keywords=('port', 'monitor', 'traffic'),
     url='https://github.com/imlonghao/portstat',
     license='Apache License 2.0',
     author='imlonghao',
     author_email='shield@fastmail.com',
     description='A simple port traffic monitor',
+    long_description=long_description,
     packages=find_packages(),
     platforms='any',
     entry_points={
