5ca8df8ab5f87725e61933bac769df3406e26fcc
imlonghao <shield@fastmail.com>
2015-06-19 15:20:21 +0800

    Disable coveralls function.

--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python
 
 from portstat import portstat
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-confPath = path.join(here, 'portstat.conf')
-
-version = portstat.version()
-portGroups = portstat.getConfig(confPath)
-portstat.sync(portGroups)
-portstat.upload(portGroups)
-
-portstat.main()
